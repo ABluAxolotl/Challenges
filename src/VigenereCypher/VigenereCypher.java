afadc49189530e991bf0345b2128fe57724fecda
Ari <rubixboy619@gmail.com>
2023-10-23 13:07:52 -0700

    Update VigenereCypher.java

--- src/VigenereCypher/VigenereCypher.java
+++ src/VigenereCypher/VigenereCypher.java
@@ -1,7 +1,6 @@
 package VigenereCypher;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class VigenereCypher {
     /*
@@ -13,14 +12,92 @@ public class VigenereCypher {
      */
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(stringToIntArray("test")));
-        System.out.println(intArrayToString(stringToIntArray("test")));
+        Scanner keyb = new Scanner(System.in);
+
+        System.out.print("Encrypting? (Y/N): ");
+        char y = keyb.next().toUpperCase().charAt(0);
+        if (y == 'Y') {
+            System.out.print("Enter the word to encrypt: ");
+            String plainText = keyb.next();
+            System.out.print("Enter the key to encrypt: ");
+            String keyWord = keyb.next();
+
+            String encrypted = vEncrypt(plainText, keyWord);
+
+            System.out.println(plainText + " encrypted with the keyword " + keyWord + " is --> " + encrypted);
+        } else if (y == 'N') {
+            System.out.print("Enter the word to decrypt: ");
+            String encrypted = keyb.next();
+            System.out.print("Enter the key to decrypt: ");
+            String keyWord = keyb.next();
+
+            String decrypted = vDecrypt(encrypted, keyWord);
+
+            System.out.println(encrypted + " decrypted with the keyword " + keyWord + " is --> " + decrypted);
+        }
+    }
+
+    public static String vEncrypt(String word, String key) {
+        word = word.toUpperCase();
+        key = key.toUpperCase();
+        int[] wordArray = stringToIntArray(word);
+        int[] keyArray = stringToIntArray(key, wordArray.length);
+
+        int[] encryptedArray = new int[wordArray.length];
+
+        wordArray = addValueToArray(wordArray, -65);
+        keyArray = addValueToArray(keyArray, -65);
+
+        for (int i = 0; i < encryptedArray.length; i++) {
+            encryptedArray[i] = wordArray[i] + keyArray[i];
+            if (encryptedArray[i] >= 26) {
+                encryptedArray[i] = encryptedArray[i] % 26;
+            }
+        }
+
+        encryptedArray = addValueToArray(encryptedArray, 65);
+
+        return intArrayToString(encryptedArray);
+    }
+
+    public static String vDecrypt(String word, String key) {
+        word = word.toUpperCase();
+        key = key.toUpperCase();
+        int[] wordArray = stringToIntArray(word);
+        int[] keyArray = stringToIntArray(key, wordArray.length);
+
+        int[] decryptedArray = new int[wordArray.length];
+
+        wordArray = addValueToArray(wordArray, -65);
+        keyArray = addValueToArray(keyArray, -65);
+
+        for (int i = 0; i < decryptedArray.length; i++) {
+            decryptedArray[i] = wordArray[i] - keyArray[i];
+            if (decryptedArray[i] < 0) {
+                decryptedArray[i] += 26;
+            }
+        }
+
+        decryptedArray = addValueToArray(decryptedArray, 65);
+
+        return intArrayToString(decryptedArray);
     }
 
     public static int[] stringToIntArray(String word) {
         int[] array = new int[word.length()];
         for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == ' ') continue;
             int asciiCode = word.charAt(i);
+            array[i] = asciiCode;
+        }
+
+        return array;
+    }
+
+    public static int[] stringToIntArray(String word, int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            int asciiCode = word.charAt(i % word.length());
             array[i] = asciiCode;
         }
 
@@ -33,5 +110,13 @@ public class VigenereCypher {
             builder.append((char) i);
         }
         return builder.toString();
+    }
+
+    public static int[] addValueToArray(int[] array, int value) {
+        int[] out = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            out[i] = array[i] + value;
+        }
+        return out;
     }
 }
