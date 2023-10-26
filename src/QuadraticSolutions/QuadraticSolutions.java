4343f8b0d9a7136f1f226989ab095cd9828d7c7f
Ari <rubixboy619@gmail.com>
2023-10-25 17:54:16 -0700

    VigenereBS

--- src/QuadraticSolutions/QuadraticSolutions.java
+++ src/QuadraticSolutions/QuadraticSolutions.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class QuadraticSolutions {
-
     public static void main(String[] args) {
         Scanner keyb = new Scanner(System.in);
 
---
+++ src/VigenereCypher/ViginereCypher.java
@@ -0,0 +1,9 @@
+package VigenereCypher;
+
+public class ViginereCypher {
+	public static void main(String[] args) {
+		int WIDTH = 320;
+		int HEIGHT = 240;
+		VigenereCypherFrame frame = new VigenereCypherFrame(WIDTH, HEIGHT);
+	}
+}
